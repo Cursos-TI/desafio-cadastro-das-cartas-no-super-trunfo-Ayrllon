7f31e8b072299fcc3cb0cf8c865c5dae56b21012
Ayrllon <151880724+Ayrllon@users.noreply.github.com>
2025-02-06 23:00:26 +0000

    Fiz o desafio mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,41 +11,97 @@ int main()
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
-     char pais[25];
-    char estado;
-    int cidade, pontosTuristicos;
-    float populacao, area, pib, perCapita, densidadePopu;
+    char pais1[25];
+    char estado1;
+    int cidade1, pontosTuristicos1;
+    float populacao1, area1, pib1, perCapta1, densidadePopu1;
+    long int superPoder1;
 
-    printf("Qual será seu pais?\n");
-    scanf("%s", &pais);
+    char pais2[25];
+    char estado2;
+    int cidade2, pontosTuristicos2;
+    float populacao2, area2, pib2, perCapta2, densidadePopu2;
+    long int superPoder2;
 
-    printf("Qual será o seu estado de A a H\n");
-    scanf(" %c", &estado);
+    printf("***************QUE COMEÇE O JOGO***************\n");
+    // carta 1
+    printf("Me diga qual o pais da carta 1\n");
+    scanf("%s", &pais1);
+
+    printf("Qual será o seu estado de A a H para a carta 1\n");
+    scanf(" %c", &estado1);
+
+    printf("Qual será a cidade de 1 a 4 para a carta 1\n");
+    scanf("%d", &cidade1);
+
+    printf("Quanto é a população é a área da cidade da carta 1\n");
+    scanf("%f %f", &populacao1, &area1);
+
+    printf("Quanto é o PIB e os pontos túristicos da cidade da carta 1\n");
+    scanf("%f %d", &pib1, &pontosTuristicos1);
+
+    perCapta1 = (pib1 / populacao1);
+    densidadePopu1 = (populacao1 / area1);
+    superPoder1 = (area1 + pib1 + pontosTuristicos1);
+
+    // carta 2
+    printf("Me diga qual o pais da carta 2\n");
+    scanf("%s", &pais2);
+
+    printf("Qual será o seu estado de A a H para a carta 2\n");
+    scanf(" %c", &estado2);
+
+    printf("Qual será a cidade de 1 a 4 para a carta 2\n");
+    scanf("%d", &cidade2);
+
+    printf("Quanto é a população é a área da cidade da carta 2\n");
+    scanf("%f %f", &populacao2, &area2);
+
+    printf("Quanto é o PIB e os pontos túristicos da cidade da carta 2\n");
+    scanf("%f %d", &pib2, &pontosTuristicos2);
+
+    perCapta2 = (pib2 / populacao2);
+    densidadePopu2 = (populacao2 / area2);
+    superPoder2 = (area2 + pib2 + pontosTuristicos2);
 
 
-    printf("Qual será a sua cidade de 1 a 4\n");
-    scanf("%d", &cidade);
+    // cartas
+    printf("********* RESULTADO *************\n");
+    // carta 1
+    printf("Pais escolhido para carta 1 foi %s\n", pais1);
 
-    printf("Quanto é a população é a área da cidade?\n");
-    scanf("%f %f", &populacao, &area);
+    printf("A carta 1 é a %c0%d\n", estado1, cidade1);
 
-    printf("Quanto é o PIB é os pontos turisticos da cidade?\n");
-    scanf("%f %d", &pib, &pontosTuristicos);
+    printf("Com população de: %.4f milhões - Área de: %.4f km²\n", populacao1, area1);
 
-    perCapita = (pib / populacao);
-    densidadePopu = (populacao / area);
+    printf("PIB: %.4f Milhões - Número de pontos turísticos: %d\n", pib1, pontosTuristicos1);
 
-    printf("**** RESULTADO ****\n");
-    printf("Pais escolhido foi o %s\n", pais);
+    printf("Com PIB per capita de: %.4f\n", perCapta1);
+    printf("Com densidade populacional de: %.4f\n", densidadePopu1);
 
-    printf("Sua carta é a %c0%d\n",estado, cidade);
+    printf("Com super poder de: %ld\n", superPoder1);
 
-    printf("Com a população de: %.4f Milhões - Área de: %.4f km²\n", populacao, area);
+    // carta 2
+    printf("Pais escolhido para carta 2 foi %s\n", pais2);
 
-    printf("PIB: %.4f Milhões - Número de pontos turísticos: %d\n", pib, pontosTuristicos);
+    printf("A carta 2 é a %c0%d\n", estado2, cidade2);
 
-    printf("Com PIB per Capita de: %.4f\n", perCapita);
-    printf("Com dencidade populacional de: %.4f\n", densidadePopu);
+    printf("Com população de: %.4f milhões - Área de: %.4f km²\n", populacao2, area2);
+
+    printf("PIB: %.4f Milhões - Número de pontos turísticos: %d\n", pib2, pontosTuristicos2);
+
+    printf("Com PIB per capita de: %.4f\n", perCapta2);
+    printf("Com densidade populacional de: %.4f\n", densidadePopu2);
+
+    printf("Com super poder de: %ld\n", superPoder2);
+
+    // resultado final
+    printf("************* RESULTADO FINAL****************\n");
+    printf("1 sendo %c0%d e 0 sendo a %c0%d\n", estado1, cidade1, estado2, cidade2);
+    printf("Qual das cartas tem mais pontos turisticos? %d\n",pontosTuristicos1 > pontosTuristicos2);
+    printf("Qual das cartas tem a maior população? %d\n",populacao1 > populacao2);
+    printf("Qual das cartas tem o PIB maior? %d\n", pib1 > pib2 );
+    printf("Qual das cartas tem o maior Super Poder? %d\n",superPoder1 > superPoder2);
 
 
 
